24c9cef4bc6af0b7adefbed37834edf8a93d7ba8
Turing-Li <382608089@qq.com>
2017-03-01 15:26:35 +0800

    对应throw测试的类新增文件提交

---
+++ TestForCLR/ExceptionThrowTestInStackTrack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForCLR
+{
+    class ExceptionThrowTestInStackTrack
+    {
+        public static void ExecptionTest()
+        {
+            new ExceptionTest().dotestForException();
+        }
+    }
+    class ExceptionTest
+    {
+        public void dotestForException()
+        { 
+            throwOneException();
+        }
+
+        public void throwOneException()
+        {
+            //intelli track工具重可以跟踪到具体抛出异常的出发点
+            throw new Exception("异常测试堆栈分析");
+        }
+    }
+}
